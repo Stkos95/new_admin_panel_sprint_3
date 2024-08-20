01e9d9160f471bee0ff6210c605b737337053bc1
stupenko <stupenko@ivk.ru>
2024-08-20 12:01:51 +0300

    add extractor

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
             counter = 0
             while True:
                 try:
-                    db.get_connection()
                     res = func(db, kwargs['statement'])
                     return res
                 except Exception as e:
@@ -27,8 +26,7 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
                     if t > border_sleep_time:
                         t = border_sleep_time
                     counter += 1
-                    time.sleep(t)
-                    
+                    time.sleep(t)          
         return inner
     return func_wrapper
 
@@ -38,28 +36,41 @@ class Database:
 
     def __init__(self, pg_data):
         self.pg_data = pg_data
+        self.conn = self.get_connection(self.pg_data)
             
 
-    @backoff()
     def make_query(self, statement: str, **kwargs):
-        cursor = self.conn.cursor()
-        cursor.execute(statement)
-        data = cursor.fetchall()
-        return data
+        while True:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(statement)
+                data = cursor.fetchall()
+                return data
+            except Exception as e:
+                print(e)
+                self.conn = self.get_connection(self.pg_data)
 
-    def get_connection(self):
-        self.conn =  psycopg.connect(**self.pg_data, row_factory=dict_row)
+    def get_connection(self, pg_data, min_time=0.1, max_time=10):
+        counter = 0
+        while True:
+            time.sleep(0.5)
+            try:
+                conn =  psycopg.connect(**pg_data, row_factory=dict_row)
+                return conn
+            except:
+                delay = min_time * 2 ** counter
+                time.sleep(delay)
+                counter += 1
 
     def close_connection(self):
         self.connection.close()
-
-
 
 
 class BaseExtractor(ABC):
 
     def __init__(self, db: Database):
         self.database = db
+        self.shift = 0
 
     @abstractmethod
     def extract_data(self):
@@ -69,8 +80,11 @@ class ExtractFilmWork(BaseExtractor):
     pass
 
 class ExtractPerson(BaseExtractor):
-    pass
-
+    def extract_data(self, limit: int = 100):
+        statement = f'select * from "content"."person" limit {limit} offset {self.shift}'
+        data = self.database.make_query(statement)
+        self.shift += limit
+        return data
 class ExtractGenre(BaseExtractor):
     pass
 
@@ -135,13 +149,17 @@ if __name__ == '__main__':
         'dbname': 'postgres',
         'user': 'postgres',
         'password': '123',
-        'host': '172.24.0.3',
+        'host': '172.18.0.2',
         'port': 5432,
     }
-    statement = 'select * from "content"."person";'
-    d = Database(dsn)
-    z = d.make_query(statement=statement)
-    print(z)
+    db = Database(pg_data=dsn)
+
+    extractor = ExtractPerson(db=db)
+    while True:
+        
+
+        z = extractor.extract_data(limit=20)
+        
 
 
 
